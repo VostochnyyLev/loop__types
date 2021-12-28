8f3c09060e1db438d6f5e8c0a7f5d59c04c91a1c
VostochnyyLev <sasha.yaremenko.00@gmail.com>
2021-12-28 15:59:18 +0200

    all loops create

---
+++ loop.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 8; i++) {
+  console.log(i);
+}
+
+let iDo = 0;
+do {
+  console.log(iDo);
+  iDo++;
+} while (iDo <= 4);
+
+let iCount = 1;
+while (iCount <= 8) {
+  console.log(iCount);
+  iCount++;
+}
